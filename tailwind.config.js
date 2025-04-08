eaba87005431e2440032de0da254e7d8df8a7df2
Jabulani Kapherachoka <jabulanikbanda769@gmail.com>
2025-04-08 23:20:02 +0200

    polishing the code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{html,js}",                // Covers all HTML/JS files in the project
-    "./src/**/*.{js,jsx,ts,tsx}",     // For React/Vite/Next.js projects
-    "./public/index.html"              // If using a public/index.html
+    "./index.html", 
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Ensure to include all file types where Tailwind classes might be used
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       boxShadow: {
         'bottom-shadow': '0 4px 10px #c7c1c1',
         'image-shadow': '0 15px 20px -5px #c7c1c1',
       },
-      colors: {  // Use `colors` instead of individual `textColor`, `borderColor`, etc.
+      colors: {  
         orange: {
           500: '#f97316',
           600: '#ea580c',
