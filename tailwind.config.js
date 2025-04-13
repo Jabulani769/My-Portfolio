8abb4975caa101e3c205304ce45db046bf678d18
Jabulani Kapherachoka <jabulanikbanda769@gmail.com>
2025-04-13 15:29:56 +0200

    added tablet view & npx updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,30 @@
 module.exports = {
   content: [
     "./index.html", 
-    "./src/**/*.{html,js,jsx,ts,tsx}", // Ensure to include all file types where Tailwind classes might be used
+    "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   darkMode: 'class',
   theme: {
+    screens: {
+      'mobile': {'max':'767px'},
+      'tablet': {'min': '768px', 'max':'1000px'},
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
+      animation: {
+        marquee: 'marquee 15s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
       boxShadow: {
         'bottom-shadow': '0 4px 10px #c7c1c1',
         'image-shadow': '0 15px 20px -5px #c7c1c1',
-      },
-      screens: {
-        sm: '640px',
       },
       colors: {  
         orange: {
